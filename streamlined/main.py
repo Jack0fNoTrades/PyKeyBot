6b5d8de011c19d58888d3eef46768b399b0b5e20
Jack0fNoTrades <bluelightningwang@gmail.com>
2025-07-08 17:32:56 -0400

    increase auto-search wait time

--- streamlined/main.py
+++ streamlined/main.py
@@ -22,13 +22,13 @@ def main():
     time.sleep(0.25)
     click(safari_icon[0], safari_icon[1], 3, 3)
     time.sleep(0.2)
-    auto_search(3, 5, safari)
+    auto_search(7, 10, safari)
     time.sleep(1)
     click(chrome_icon[0], chrome_icon[1], 3, 3)
     time.sleep(0.7)
     click(1230, 180, 5, 5)
     time.sleep(3)
-    auto_search(3, 5, chrome)
+    auto_search(7, 10, chrome)
 
 
 # The schedule library runs the program immediately and our needs differ slightly from
